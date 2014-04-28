58548192bae072b3ac75c6503e163b2f55841c60
Rich Wareham <rjw57@cam.ac.uk>
2014-04-28 16:58:04 +0100

    wrap app in a google account controller

---
+++ app/scripts/controllers/googleaccount.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('webappApp')
+  .controller('GoogleAccountCtrl', function ($scope, GoogleApi) {
+    var updateTokens = function() {
+      console.log('Google API token state changed');
+      console.log('Token:', GoogleApi.accessToken);
+      console.log('Signed in:', !!GoogleApi.accessToken);
+      $scope.accessToken = GoogleApi.accessToken;
+      $scope.accessTokenExpiry = GoogleApi.expiryDate;
+      $scope.isSignedIn = !!GoogleApi.accessToken;
+    };
+
+    $scope.$on('googleApiStateChanged', updateTokens);
+    $scope.doLogin = function() { GoogleApi.login(); }
+    $scope.doLogout = function() { GoogleApi.logout(); }
+
+    $scope.$watch('isSignedIn', function() {
+      if(!$scope.isSignedIn) {
+        $scope.me = null;
+        return;
+      }
+
+      // get user info when we're signed in
+      GoogleApi.get('https://www.googleapis.com/plus/v1/people/me')
+        .success(function(data) { $scope.me = data; });
+    });
+
+    updateTokens();
+  });
--- app/scripts/services/googleapi.js
+++ app/scripts/services/googleapi.js
@@ -2,62 +2,119 @@
 
 // A service to manage logging in and out of the Google API.
 angular.module('webappApp')
-  .service('GoogleApi', function($rootScope, $http, Token) {
+  .service('GoogleApi', function($http, $q, $rootScope, Token) {
     var GoogleApi = function() {
-      $rootScope.accessToken = Token.get();
+      this.accessToken = null;
+      this.expiryDate = null;
+      if(Token.get()) { this._verifyTokenAsync(Token.get()); }
+      $rootScope.$broadcast('googleApiStateChanged');
+    };
+
+    // Start the verification process for a token returning a promise
+    // which is fulfilled if the token verification succeeds otherwise
+    // it fails.
+    var verifyToken = function(token) {
+      /* jshint camelcase: false */
+      var deferred = $q.defer();
+
+      console.log('Asked to verify:', token);
+      if(token) {
+        Token.verifyAsync(token)
+          .then(function(params) {
+            console.log('Token verification succeeded:', params);
+            deferred.resolve(angular.extend({ accessToken: token }, params));
+          }, function(err) {
+            // FIXME: add diagnostics box
+            deferred.reject(err);
+          });
+      } else {
+        deferred.reject();
+      }
+
+      return deferred.promise;
+    };
+
+    // Verify token, update accessToken, etc appropriately and then broadcast
+    // googleApiStateChanged on the rootScope.
+    GoogleApi.prototype._verifyTokenAsync = function(token) {
+      var self = this;
+
+      verifyToken(token)
+        .then(function(params) {
+
+          // success
+          self.accessToken = params.accessToken;
+          self.expiryDate = new Date(Date.now() + (1000 * params.expires_in));
+
+          Token.set(token);
+          $rootScope.$broadcast('googleApiStateChanged');
+
+          console.log('New token set with expiry date:', self.expiryDate);
+        }, function() {
+          // failure
+          self.accessToken = null;
+          self.expiryDate = null;
+          Token.clear();
+          $rootScope.$broadcast('googleApiStateChanged');
+        });
     };
 
     GoogleApi.prototype.login = function() {
-      /* global alert */
-      /* jshint camelcase: false */
+      var self = this;
+      var deferred = $q.defer();
+
       Token.getTokenByPopup()
         .then(function(params) {
           // Success getting token from popup.
 
           // Verify the token before setting it, to avoid the confused deputy problem.
-          Token.verifyAsync(params.access_token).
-            then(function() {
-              $rootScope.$apply(function() {
-                $rootScope.accessToken = params.access_token;
-                $rootScope.expiresIn = params.expires_in;
-                Token.set(params.access_token);
-              });
-            }, function() {
-              // FIXME: add diagnostics box
-              alert('Failed to verify token.');
-            });
-
-        }, function() {
+          self._verifyTokenAsync(params.access_token);
+        }, function(err) {
           // FIXME: add diagnostics box
           // Failure getting token from popup.
-          alert('Failed to get token from popup.');
+          deferred.reject(err);
         });
+
+      return deferred.promise;
     };
 
     // unauthenticating is a *lot* easier :)
     GoogleApi.prototype.logout = function() {
-      $rootScope.accessToken = null;
+      this.accessToken = null;
+      this.expiresIn = null;
       Token.clear();
+      $rootScope.$broadcast('googleApiStateChanged');
+    };
+
+    GoogleApi.prototype.tokenHasExpired = function() {
+      return Date.now() >= this.expiryDate.getTime();
+    };
+
+    GoogleApi.prototype.isSignedIn = function() {
+      return !!this.accessToken && !this.tokenHasExpired();
     };
 
     // Make a request, authenticated if possible
     GoogleApi.prototype.http = function(config) {
+      var self = this;
+
       // copy config
       var fullConfig = angular.extend({ headers: {} }, config);
 
       // set headers
-      if($rootScope.accessToken) {
+      if(self.accessToken) {
         fullConfig.headers['Authorization'] = // jshint ignore:line
-          'Bearer ' + $rootScope.accessToken;
+          'Bearer ' + self.accessToken;
       }
 
-      return $http(fullConfig);
+      return $http(fullConfig)
     };
 
     // Convenience wrappers
     GoogleApi.prototype.get = function(url, config) {
       return this.http(angular.extend({ method: 'GET', url: url }, config));
     };
+
     GoogleApi.prototype.post = function(url, data, config) {
       return this.http(angular.extend({ method: 'POST', url: url, data: data }, config));
     };
