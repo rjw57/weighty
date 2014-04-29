04a707f18afd8f7f4f8a6409c01ced12ea2e53e8
Rich Wareham <rjw57@cam.ac.uk>
2014-04-29 13:02:21 +0100

    move dataset list to left

--- app/scripts/controllers/googleaccount.js
+++ app/scripts/controllers/googleaccount.js
@@ -44,7 +44,7 @@ angular.module('webappApp')
     };
 
     // Try an immediate-mode login when the authorization system is ready
-    gapi.auth.init().then(function() {
+    gapi.ready(function() {
       $scope.doLogin({ immediate: true });
     });
 
