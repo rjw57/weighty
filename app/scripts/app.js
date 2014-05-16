395aaf7e08580b2e88210232e41f4154ccdc106f
Rich Wareham <rjw57@cam.ac.uk>
2014-05-16 14:31:18 +0100

    add dataset route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,10 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
+      .when('/dataset/:datasetId', {
+        templateUrl: 'views/dataset.html',
+        controller: 'DatasetController'
+      })
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
